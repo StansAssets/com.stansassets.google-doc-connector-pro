2c610982475b3de3695f5fa36c2102bdce7d8e10
Alexey Yaremenko <alex@stansassets.com>
2020-08-22 21:36:29 +0300

    the whole spreadsheet loading is wrapped with try/catch block. added empty sheet checks

--- Editor/Core/SpreadsheetLoader.cs
+++ Editor/Core/SpreadsheetLoader.cs
@@ -69,115 +69,119 @@ namespace StansAssets.GoogleDoc
             try
             {
                 spreadsheetData = await rangeRequest.ExecuteAsync();
+
+                m_Spreadsheet.SyncDateTime = DateTime.Now;
+                m_Spreadsheet.SetName(spreadsheetData.Properties.Title);
+
+                string projectRootPath = Application.dataPath.Substring(0, Application.dataPath.Length - 6);
+                var spreadsheetPath = Path.Combine(projectRootPath, GoogleDocConnectorSettings.Instance.SpreadsheetsFolderPath, m_Spreadsheet.Name);
+                m_Spreadsheet.SetPath(spreadsheetPath);
+                m_Spreadsheet.SetMachineName(SystemInfo.deviceName);
+                m_Spreadsheet.SetUrl(spreadsheetData.SpreadsheetUrl);
+                m_Spreadsheet.CleanupSheets();
+
+                //Set Sheets
+                foreach (var sheetData in spreadsheetData.Sheets)
+                {
+                    var sheetId = sheetData.Properties.SheetId ?? 0;
+                    var sheet = m_Spreadsheet.CreateSheet(sheetId, sheetData.Properties.Title);
+                    sheet.CleanupRows();
+
+                    // We always request the whole grid to get full data set.
+                    // So there is only one item that represents the whole sheet.
+                    var gridData = sheetData.Data[0];
+                    if (gridData?.RowData == null)
+                        continue;
+
+                    var rowIndex = 0;
+                    foreach (var rowData in gridData.RowData)
+                    {
+                        var row = new RowData();
+                        if (rowData.Values != null)
+                        {
+                            var columnIndex = 0;
+                            foreach (var cellData in rowData.Values)
+                            {
+                                if (cellData.FormattedValue == null)
+                                {
+                                    continue;
+                                }
+
+                                string stringValue;
+                                if (cellData.EffectiveValue.BoolValue != null)
+                                {
+                                    stringValue = cellData.EffectiveValue.BoolValue.ToString();
+                                }
+                                else if (cellData.EffectiveValue.NumberValue != null)
+                                {
+                                    stringValue = cellData.EffectiveValue.NumberValue.ToString();
+                                }
+                                else
+                                {
+                                    stringValue = cellData.EffectiveValue.StringValue;
+                                }
+
+                                var cellValue = new CellValue(
+                                    cellData.FormattedValue,
+                                    cellData.EffectiveValue.FormulaValue,
+                                    stringValue);
+
+                                var cell = new Cell(rowIndex, columnIndex, cellValue);
+                                row.AddCell(cell);
+
+                                columnIndex++;
+                            }
+                        }
+
+                        sheet.AddRow(row);
+
+                        rowIndex++;
+                    }
+                }
+
+                //Set NamedRanges
+                if (spreadsheetData.NamedRanges != null)
+                {
+                    foreach (var namedRangeData in spreadsheetData.NamedRanges)
+                    {
+                        var sheetId = namedRangeData.Range.SheetId ?? 0;
+                        var sheet = m_Spreadsheet.GetSheet(sheetId);
+                        var namedRange = sheet.CreateNamedRange(namedRangeData.NamedRangeId, namedRangeData.Name);
+                        var range = namedRangeData.Range;
+
+                        Debug.Assert(range.StartRowIndex != null, "range.StartRowIndex != null");
+                        Debug.Assert(range.EndRowIndex != null, "range.EndRowIndex != null");
+                        Debug.Assert(range.StartColumnIndex != null, "range.StartColumnIndex != null");
+                        Debug.Assert(range.EndColumnIndex != null, "range.EndColumnIndex != null");
+
+                        var cells = new List<Cell>();
+                        for (var i = range.StartRowIndex.Value; i < range.EndRowIndex.Value; i++)
+                        {
+                            for (var j = range.StartColumnIndex.Value; j < range.EndColumnIndex.Value; j++)
+                            {
+                                var cell = new Cell(i, j);
+                                cells.Add(cell);
+                            }
+                        }
+
+                        namedRange.SetCells(cells);
+                    }
+                }
+
+                if (!Directory.Exists(GoogleDocConnectorSettings.Instance.SpreadsheetsFolderPath))
+                    Directory.CreateDirectory(GoogleDocConnectorSettings.Instance.SpreadsheetsFolderPath);
+
+                File.WriteAllText(spreadsheetPath, JsonConvert.SerializeObject(m_Spreadsheet));
+                m_Spreadsheet.ChangeStatus(Spreadsheet.SyncState.Synced);
             }
             catch (GoogleApiException exception)
             {
                 SetSpreadsheetSyncError(m_Spreadsheet, exception.Error.Message);
-                return;
             }
             catch (Exception exception)
             {
                 SetSpreadsheetSyncError(m_Spreadsheet, exception.Message);
-                return;
             }
-
-            m_Spreadsheet.SyncDateTime = DateTime.Now;
-            m_Spreadsheet.SetName(spreadsheetData.Properties.Title);
-
-            string projectRootPath = Application.dataPath.Substring(0, Application.dataPath.Length - 6);
-            var spreadsheetPath = Path.Combine(projectRootPath, GoogleDocConnectorSettings.Instance.SpreadsheetsFolderPath, m_Spreadsheet.Name);
-            m_Spreadsheet.SetPath(spreadsheetPath);
-            m_Spreadsheet.SetMachineName(SystemInfo.deviceName);
-            m_Spreadsheet.SetUrl(spreadsheetData.SpreadsheetUrl);
-            m_Spreadsheet.CleanupSheets();
-
-            //Set Sheets
-            foreach (var sheetData in spreadsheetData.Sheets)
-            {
-                var sheetId = sheetData.Properties.SheetId ?? 0;
-                var sheet = m_Spreadsheet.CreateSheet(sheetId, sheetData.Properties.Title);
-                sheet.CleanupRows();
-
-                var gridData = sheetData.Data[0];
-                var rowIndex = 0;
-                foreach (var rowData in gridData.RowData)
-                {
-                    var row = new RowData();
-                    if (rowData.Values != null)
-                    {
-                        var columnIndex = 0;
-                        foreach (var cellData in rowData.Values)
-                        {
-                            if (cellData.FormattedValue == null)
-                            {
-                                continue;
-                            }
-
-                            string stringValue;
-                            if (cellData.EffectiveValue.BoolValue != null)
-                            {
-                                stringValue = cellData.EffectiveValue.BoolValue.ToString();
-                            } else if (cellData.EffectiveValue.NumberValue != null)
-                            {
-                                stringValue = cellData.EffectiveValue.NumberValue.ToString();
-                            }
-                            else
-                            {
-                                stringValue = cellData.EffectiveValue.StringValue;
-                            }
-                            
-                            var cellValue = new CellValue(
-                                cellData.FormattedValue,
-                                cellData.EffectiveValue.FormulaValue,
-                                stringValue);
-
-                            var cell = new Cell(rowIndex, columnIndex, cellValue);
-                            row.AddCell(cell);
-
-                            columnIndex++;
-                        }
-                    }
-
-                    sheet.AddRow(row);
-
-                    rowIndex++;
-                }
-            }
-
-            //Set NamedRanges
-            if (spreadsheetData.NamedRanges != null)
-            {
-                foreach (var namedRangeData in spreadsheetData.NamedRanges)
-                {
-                    var sheetId = namedRangeData.Range.SheetId ?? 0;
-                    var sheet = m_Spreadsheet.GetSheet(sheetId);
-                    var namedRange = sheet.CreateNamedRange(namedRangeData.NamedRangeId, namedRangeData.Name);
-                    var range = namedRangeData.Range;
-
-                    Debug.Assert(range.StartRowIndex != null, "range.StartRowIndex != null");
-                    Debug.Assert(range.EndRowIndex != null, "range.EndRowIndex != null");
-                    Debug.Assert(range.StartColumnIndex != null, "range.StartColumnIndex != null");
-                    Debug.Assert(range.EndColumnIndex != null, "range.EndColumnIndex != null");
-
-                    var cells = new List<Cell>();
-                    for (var i = range.StartRowIndex.Value; i < range.EndRowIndex.Value; i++)
-                    {
-                        for (var j = range.StartColumnIndex.Value; j < range.EndColumnIndex.Value; j++)
-                        {
-                            var cell = new Cell(i, j);
-                            cells.Add(cell);
-                        }
-                    }
-
-                    namedRange.SetCells(cells);
-                }
-            }
-
-            if (!Directory.Exists(GoogleDocConnectorSettings.Instance.SpreadsheetsFolderPath))
-                Directory.CreateDirectory(GoogleDocConnectorSettings.Instance.SpreadsheetsFolderPath);
-
-            File.WriteAllText(spreadsheetPath, JsonConvert.SerializeObject(m_Spreadsheet));
-            m_Spreadsheet.ChangeStatus(Spreadsheet.SyncState.Synced);
         }
 
         void SetSpreadsheetSyncError(Spreadsheet spreadsheet, string exceptionMessage)
