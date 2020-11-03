147dfe4906c9d628100527e1e75c5beeb262c542
Rufield <biglazycatukr@gmail.com>
2020-11-04 00:12:34 +0200

    fix: Binding LocalizedLabel to LocalizedLabelEditorUI

--- Editor/Localization/UIE/LocalizedLabelEditorUI.cs
+++ Editor/Localization/UIE/LocalizedLabelEditorUI.cs
@@ -10,7 +10,6 @@ using UnityEngine.UIElements;
 
 namespace StansAssets.GoogleDoc
 {
-
     public class LocalizedLabelEditorUI : BaseTab
     {
         readonly VisualElement m_Root;
@@ -18,33 +17,31 @@ namespace StansAssets.GoogleDoc
         HelpBox m_ErrorHelpBox;
 
         readonly LocalizedLabel m_LocalizedLabel;
-        
+
         public LocalizedLabelEditorUI(LocalizedLabel localizedLabel)
             : base($"{GoogleDocConnectorPackage.UILocalizationPath}/LocalizedLabelEditorUI")
         {
             m_LocalizedLabel = localizedLabel;
             m_Root = this.Q<VisualElement>("LocalizedLabelEditorRoot");
+
             GoogleDocConnectorLocalization.SpreadsheetIdChanged += Bind;
             try
             {
-                LocalizationClient.Default.OnLanguageChanged += () =>
-                {
-                    Bind();
-                    UpdateLocalization("", "");
-                };
+                LocalizationClient.Default.OnLanguageChanged += UpdateLocalization;
             }
-            catch
+            catch (Exception exception)
             {
-                UpdateLocalizationError("There are errors in LocalizationClient, more detailed on Localization Tab");
+                UpdateLocalizationError(exception.Message);
                 return;
             }
+
             Bind();
         }
 
         void Bind()
         {
             m_Root.Clear();
-            PropertyField("Token Id", m_LocalizedLabel.m_Token.Token);
+            PropertyField("Token Id", "m_Token.m_TokenId");
             try
             {
                 var values = LocalizationClient.Default.Sheets;
@@ -52,7 +49,8 @@ namespace StansAssets.GoogleDoc
                 {
                     m_LocalizedLabel.m_Token.SectionSet(values.First());
                 }
-                PropertyPopup("Section", m_LocalizedLabel.m_Token.Section, LocalizationClient.Default.Sheets);
+
+                PropertyPopup("Section", "m_Token.m_Section", LocalizationClient.Default.Sheets);
             }
             catch
             {
@@ -60,58 +58,45 @@ namespace StansAssets.GoogleDoc
                 UpdateLocalizationError("There are errors in LocalizationClient, more detailed on Localization Tab");
                 return;
             }
-            PropertyPopup("Text Type", m_LocalizedLabel.m_Token.TextType.ToString(), Enum.GetNames(typeof(TextType)).ToList());
-            PropertyField("Prefix", m_LocalizedLabel.m_Token.Prefix);
-            PropertyField("Suffix", m_LocalizedLabel.m_Token.Suffix);
+
+            PropertyPopup("Text Type", "m_Token.m_TextType", Enum.GetNames(typeof(TextType)).ToList());
+            PropertyField("Prefix", "m_Token.m_Prefix");
+            PropertyField("Suffix", "m_Token.m_Suffix");
             InitErrorHelpBox();
-            var labelLang = new Label() { text= "Available Languages:"};
+            var labelLang = new Label() { text = "Available Languages:" };
             labelLang.AddToClassList("header-lang");
             m_Root.Add(labelLang);
             m_ListLang = new VisualElement();
             m_ListLang.AddToClassList("list-lang");
             m_Root.Add(m_ListLang);
             SelectedLang(LocalizationClient.Default.CurrentLanguage);
+
+            var so = new SerializedObject(m_LocalizedLabel);
+            m_Root.Bind(so);
         }
 
-        void PropertyField(string nameProperty, string property)
+        void PropertyField(string propertyName, string bindingPath)
         {
-            var propertyField = new TextField(nameProperty) { value = property };
-            propertyField.RegisterCallback<ChangeEvent<string>>((ev) =>
+            var propertyField = new TextField(propertyName) { bindingPath = bindingPath };
+            propertyField.RegisterCallback<KeyUpEvent>((ev) =>
             {
-                UpdateLocalization(nameProperty, ev.newValue);
-            });
-            m_Root.Add(propertyField);
-        }
-        void PropertyPopup(string nameProperty, string property, List<string> values)
-        {
-            var propertyField = new PopupField<string>(nameProperty, values, 0) { value = property };
-            propertyField.RegisterCallback<ChangeEvent<string>>((ev) =>
-            {
-                UpdateLocalization(nameProperty, ev.newValue);
+                UpdateLocalization();
             });
             m_Root.Add(propertyField);
         }
 
-        void UpdateLocalization(string nameProperty, string newValue)
+        void PropertyPopup(string propertyName, string bindingPath, List<string> values)
         {
-            switch (nameProperty)
+            var propertyField = new PopupField<string>(propertyName, values, 0) { bindingPath = bindingPath };
+            propertyField.RegisterCallback<MouseDownEvent>((ev) =>
             {
-                case "Token Id":
-                    m_LocalizedLabel.m_Token.TokenSet(newValue);
-                    break;
-                case "Section":
-                    m_LocalizedLabel.m_Token.SectionSet(newValue);
-                    break;
-                case "Text Type":
-                    m_LocalizedLabel.m_Token.TextTypeSet((TextType)Enum.Parse(typeof(TextType), newValue));
-                    break;
-                case "Prefix":
-                    m_LocalizedLabel.m_Token.PrefixSet(newValue);
-                    break;
-                case "Suffix":
-                    m_LocalizedLabel.m_Token.SuffixSet(newValue);
-                    break;
-            }
+                schedule.Execute(UpdateLocalization).StartingIn(5);
+            });
+            m_Root.Add(propertyField);
+        }
+
+        void UpdateLocalization()
+        {
             try
             {
                 UpdateLocalizationError(string.Empty);
@@ -129,15 +114,17 @@ namespace StansAssets.GoogleDoc
             {
                 InitErrorHelpBox();
             }
+
             if (string.IsNullOrEmpty(error))
             {
                 m_ErrorHelpBox.style.display = DisplayStyle.None;
                 return;
             }
-            
+
             m_ErrorHelpBox.Text = error;
             m_ErrorHelpBox.style.display = DisplayStyle.Flex;
         }
+
         void SelectedLang(string langNew)
         {
             var languages = LocalizationClient.Default.Languages;
