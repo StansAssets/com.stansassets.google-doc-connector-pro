0ebfc8011b0818380a52602e97f5ce3e49298de2
Stanislav Osipov <lacost.st@gmail.com>
2021-01-10 21:05:12 +0200

    chore: InternalsVisibleTo("StansAssets.Build.Meta.Editor")

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("StansAssets.Build.Editor")]
+[assembly: InternalsVisibleTo("StansAssets.Build.Meta.Editor")]
 [assembly: InternalsVisibleTo("StansAssets.GoogleDoc.Tests")]
