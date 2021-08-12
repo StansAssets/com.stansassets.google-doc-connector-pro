abb949cd9bc8d705de54da335495390e397fa80b
Koretsky Konstantin <frostinglg@gmail.com>
2021-08-12 13:49:33 +0300

    feat: update xml comment for test update yml documentation

--- com.stansassets.google-doc-connector-pro/Runtime/Interfaces/ICellPointer.cs
+++ com.stansassets.google-doc-connector-pro/Runtime/Interfaces/ICellPointer.cs
@@ -2,7 +2,14 @@ namespace StansAssets.GoogleDoc
 {
     public interface ICellPointer
     {
+        /// <summary>
+        /// Cell row
+        /// </summary>
         int Row { get; }
+        
+        /// <summary>
+        /// Cell column
+        /// </summary>
         int Column { get; }
     }
 }
